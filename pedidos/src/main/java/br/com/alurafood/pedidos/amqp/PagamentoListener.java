fe5c071273f498edaa4b8eab4ac03b5bb7331a8e
William Mian <william_mian@yahoo.com.br>
2022-10-27 08:25:57 -0300

    manipulando os dados recebidos

--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,15 +1,25 @@
 package br.com.alurafood.pedidos.amqp;
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import br.com.alurafood.pedidos.dto.PagamentoDto;
 
 @Component
 public class PagamentoListener {
 
 	@RabbitListener(queues = "pagamento.concluido")
-	public void recebeMensagem(Message mensagem) {
-		System.out.println("Recebi a mensagem: " + mensagem.toString());
+	public void recebeMensagem(PagamentoDto pagamento) {
+		String mensagem = """
+				Dados do pagamento: %s
+				Número do pedido: %s
+				Valor R$: %s
+				""".formatted(pagamento.getId(), 
+						pagamento.getPedidoId(),
+						pagamento.getValor(), 
+						pagamento.getStatus());
+		
+		System.out.println("Recebi a mensagem: " + mensagem);
 	}
 	
 }
